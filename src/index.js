e3ca51ccab933ef9f4c2db7f8ef54408c01f6525
Benny Chrispin <ndizibaidu23@gmail.com>
2023-11-30 13:15:36 +0200

    curring in redux

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ let input = " Javascript";
 let output = "<div>" + input.trim() + "</div>";
 
 const trim = (str) => str.trim();
-const wrapInDiv = (str) => `<div>${str}</div>`;
-const wrap = (type, str) => `<${type}> ${str}</${type}`;
+wrap = (type) => (str) => `<${type}> ${str}</${type}`;
 const toLowerCase = (str) => str.toLowerCase();
 
 const transform = pipe(trim, toLowerCase, wrap("div"));
